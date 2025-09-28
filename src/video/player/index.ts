17d17cd050be210ad6aa65aac02bb047e621e507
wuhaolei <wuhaolei@kanyun.com>
2025-09-29 00:55:11 +0800

    Revert "another video cover"
    This reverts commit 4fc8a881940b0d1d41d83b4342524d72a0d449ae.

--- src/video/player/index.ts
+++
@@ -1,6 +0,0 @@
-// 视频播放器模块导出
-export { VideoPlayerController, PlayerEvent, PlayerState } from './VideoPlayerController';
-export type { IPlayerController } from './VideoPlayerController';
-
-export { LeoVideoPlayer } from './LeoVideoPlayer';
-export type { LeoVideoPlayerConfig, LeoVideoPlayerProps } from './LeoVideoPlayer';
